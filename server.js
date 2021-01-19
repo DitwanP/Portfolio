55acc598ce69ca8192c6df62a94749ffb65953c2
DitwanP <price.ditwan@gmail.com>
2021-01-18 22:53:16 -0500

    misc

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const sgTransport = require('nodemailer-sendgrid-transport');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 const transporter = nodemailer.createTransport(sgTransport({
     auth: {
@@ -58,8 +72,4 @@ router.post('/send', (req, res, next) => {
     })
 });
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.listen(PORT)
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
