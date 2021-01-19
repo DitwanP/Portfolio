5abb99ced8d4e17c5f4cc4c54211ec828e991661
DitwanP <price.ditwan@gmail.com>
2021-01-18 23:29:37 -0500

    added build folder

---
+++ build/precache-manifest.9ac490b5744093eaa19fb2241e17dea7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31337b9f342b88a4139ea45eeb87e8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86b67cdbbf8d2566326f",
+    "url": "/static/css/6.0f496660.chunk.css"
+  },
+  {
+    "revision": "8b4f57f6f72eef90baec",
+    "url": "/static/css/7.859b8e53.chunk.css"
+  },
+  {
+    "revision": "c5b1fc17e0877f92f5bd",
+    "url": "/static/css/8.b4b0c891.chunk.css"
+  },
+  {
+    "revision": "7345fb350a25c7e9b410",
+    "url": "/static/css/9.9c2a782d.chunk.css"
+  },
+  {
+    "revision": "18b132d0768506a52da1",
+    "url": "/static/css/main.7524dd6b.chunk.css"
+  },
+  {
+    "revision": "79afcd91a2d53a4fd13e",
+    "url": "/static/js/0.e6d74b99.chunk.js"
+  },
+  {
+    "revision": "c3697635217a4bc8d1c349b6c27499c3",
+    "url": "/static/js/0.e6d74b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f7b9a7d461ed0dc07f",
+    "url": "/static/js/1.8d512f1a.chunk.js"
+  },
+  {
+    "revision": "405278be2563551e7808",
+    "url": "/static/js/10.ae9b4afe.chunk.js"
+  },
+  {
+    "revision": "bfd774368a763026b789",
+    "url": "/static/js/11.d378e61a.chunk.js"
+  },
+  {
+    "revision": "83c3b679bb599fb88a93",
+    "url": "/static/js/4.1b03e232.chunk.js"
+  },
+  {
+    "revision": "b5ff4111ab81c8c91e9e746653eb6fff",
+    "url": "/static/js/4.1b03e232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084e88e0485a3a79bdda",
+    "url": "/static/js/5.a21c613d.chunk.js"
+  },
+  {
+    "revision": "86b67cdbbf8d2566326f",
+    "url": "/static/js/6.603363a4.chunk.js"
+  },
+  {
+    "revision": "8b4f57f6f72eef90baec",
+    "url": "/static/js/7.8bb5862b.chunk.js"
+  },
+  {
+    "revision": "c5b1fc17e0877f92f5bd",
+    "url": "/static/js/8.85de78c5.chunk.js"
+  },
+  {
+    "revision": "7345fb350a25c7e9b410",
+    "url": "/static/js/9.236b258d.chunk.js"
+  },
+  {
+    "revision": "18b132d0768506a52da1",
+    "url": "/static/js/main.af2a40d2.chunk.js"
+  },
+  {
+    "revision": "7fd8e347040bccac672f",
+    "url": "/static/js/runtime-main.9fca2a6e.js"
+  },
+  {
+    "revision": "9888ddeb71e921b13a2b162298926be6",
+    "url": "/static/media/bookstore.9888ddeb.mp4"
+  },
+  {
+    "revision": "3f1c0fba7836bc6fc9bc6c35af0caa92",
+    "url": "/static/media/homebase.3f1c0fba.mp4"
+  },
+  {
+    "revision": "0813ff01b9120c33667b10be117c4a36",
+    "url": "/static/media/statify.0813ff01.mp4"
+  }
+]);
--- server.js
+++ server.js
@@ -9,12 +9,21 @@ const sgTransport = require('nodemailer-sendgrid-transport');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 const transporter = nodemailer.createTransport(sgTransport({
     auth: {
